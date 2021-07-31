29bd9a747e7a9d334f5597399154ca143b3b6739
bwireman <bwireman1@gmail.com>
2021-07-31 14:35:28 -0400

    adds dialyxir to test env for ci

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule CensysEx.MixProject do
       {:timex, "~> 3.7"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:mox, "~> 1.0", only: :test},
-      {:dialyxir, "~> 1.1", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.25.0", only: :dev, runtime: false}
     ]
   end
