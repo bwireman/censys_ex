19258d166adbae74735b3085048ec1d9165e0c98
bwireman <bwireman1@gmail.com>
2021-08-05 21:10:07 -0400

    adds experimental host_events API support

---
+++ lib/collections/expiremental.ex
@@ -0,0 +1,32 @@
+defmodule CensysEx.Experimental do
+  @moduledoc """
+  CensysEx wrapper for the search.censys.io v2 "experimental" APIs.
+  *_NOTE_*: these APIs are subject to change in the future and CensysEx may get out of sync with Censys
+  """
+
+  alias CensysEx.{Paginate, Util}
+
+  @doc """
+  Hits the Experimental Censys host events API
+
+    - API docs: https://search.censys.io/api#/experimental/viewHostEvents
+
+  ## Examples
+
+  ```
+  CensysEx.Experimental.host_events("127.0.0.1")
+  |> Stream.take(25)
+  |> Stream.map(&Map.get(&1, "_event"))
+  |> Enum.to_list()
+  ["service_observed", "location_updated", ...]
+  ```
+  """
+  @spec host_events(String.t(), integer(), boolean(), DateTime.t() | nil, DateTime.t() | nil) ::
+          CensysEx.result_stream(map())
+  def host_events(ip, per_page \\ 50, reversed \\ false, start_time \\ nil, end_time \\ nil) do
+    next = fn params -> Util.get_client().get("experimental", "hosts/#{ip}/events", [], params) end
+    extractor = fn client = %Paginate{} -> get_in(client.results, ["result", "events"]) end
+
+    Paginate.stream(next, extractor, Util.build_experimental_get_host_events(per_page, reversed, start_time, end_time))
+  end
+end
